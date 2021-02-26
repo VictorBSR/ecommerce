84805cfc51dc48c86c1bef34f6637f03a12075f6
Victor Reis <victorbsreis@gmail.com>
2021-02-26 15:17:18 -0300

    início do projeto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b0dcdb06ddbbf1feca2cf94c977b84d158e99814',
+    'name' => 'victorbsr/ecommerce',
+  ),
+  'versions' => 
+  array (
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v5.2.22',
+      'version' => '5.2.22.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b18cb98131bd83103ccb26a888fdfe3177b8a663',
+    ),
+    'rain/raintpl' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5bfdf4602b8adb0e80b222fa29ac559aa9c06abf',
+    ),
+    'slim/slim' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+    ),
+    'victorbsr/ecommerce' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b0dcdb06ddbbf1feca2cf94c977b84d158e99814',
+    ),
+  ),
+);
