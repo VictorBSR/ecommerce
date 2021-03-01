c422d2a3a0d98142ce374e63ebcbc7140b6dae2c
Victor Reis <victorbsreis@gmail.com>
2021-03-01 15:39:23 -0300

    login e logout

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
-//use \Slim\Slim;
-//use \Hcode\Page;
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -15,6 +17,42 @@ $app->get('/', function() {
 	$page = new Hcode\Page();
 	$page->setTpl("index");
 
+});
+
+$app->get('/admin', function() {
+
+	User::verifyLogin();
+    
+	$page = new Hcode\PageAdmin();
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+    
+	//User::login($_POST["login"],$_POST["password"]);
+	$usr = new Hcode\Model\User();
+	$usr->login($_POST["login"],$_POST["password"]);
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
