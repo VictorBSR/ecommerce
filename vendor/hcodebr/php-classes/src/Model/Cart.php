2a23f30d852651461a87874cca479333f413a66c
Victor Reis <victorbsreis@gmail.com>
2021-05-23 00:06:09 -0300

    Correção bugs cart e frete

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -143,8 +143,8 @@ class Cart extends Model {
 
         $sql = new Sql();
         $results = $sql->select("
-            SELECT SUM(vlprice) AS vlprice, SUM(vlwidth) AS vlwidth, SUM(vlheight) AS vlheigth, SUM(vllength) AS vllength, SUM(
-                vlweigth) AS vlweigth, COUNT(*) AS nrqtd
+            SELECT SUM(vlprice) AS vlprice, SUM(vlwidth) AS vlwidth, SUM(vlheight) AS vlheigth, SUM(vllength) AS vllength, 
+                SUM(vlweight) AS vlweight, COUNT(*) AS nrqtd
             FROM tb_products a
             INNER JOIN tb_cartsproducts b ON a.idproduct = b.idproduct
             WHERE b.idcart = :idcart AND dtremoved IS NULL;
