c58980e6d4221373218a73f1036b6ca208b8f661
Victor Reis <victorbsreis@gmail.com>
2021-06-05 23:17:23 -0300

    CEP e erros

--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -50,10 +50,10 @@ class Address extends Model {
         $results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
             ':idaddress'=>$this->getidaddress(),
             ':idperson'=>$this->getidperson(),
-            ':desaddress'=>$this->getdesaddress(),
-            ':descomplement'=>$this->getdescomplement(),
-            ':descity'=>$this->getdescity(),
-            ':desstate'=>$this->getdesstate(),
+            ':desaddress'=>utf8_decode($this->getdesaddress()),
+            ':descomplement'=>utf8_decode($this->getdescomplement()),
+            ':descity'=>utf8_decode($this->getdescity()),
+            ':desstate'=>utf8_decode($this->getdesstate()),
             ':descountry'=>$this->getdescountry(),
             ':deszipcode'=>$this->getdeszipcode(),
             ':desdistrict'=>$this->getdesdistrict()
